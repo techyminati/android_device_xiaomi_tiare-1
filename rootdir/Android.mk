ece01544037ed4470c91ab9d7d3ff70abba96cc5
zqm64 <zqm64@outlook.com>
2020-11-17 09:42:09 +0800

    tiare: rootdir: Decommonize fstab.qcom for tiare
    Updated zram size, and use vendor partition as /vendor

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -1,5 +1,13 @@
 LOCAL_PATH := $(call my-dir)
 
+include $(CLEAR_VARS)
+LOCAL_MODULE       := fstab.qcom
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := fstab.qcom
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)
+include $(BUILD_PREBUILT)
+
 # Device Script
 include $(CLEAR_VARS)
 LOCAL_MODULE       := init.tiare.rc
