304bb787884bfcde775e959f1101db669093d8fd
zqm64 <zqm64@outlook.com>
2020-11-18 19:15:05 +0800

    tiare: Replace libgui with libwui for camera blob
    Co-authored-by: Erfan Abdi <erfangplus@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -21,3 +21,6 @@ export VENDOR=xiaomi
 export DEVICE_BRINGUP_YEAR=2017
 
 "./../../${VENDOR}/${DEVICE_COMMON}/extract-files.sh" "$@"
+
+# Wrap libgui_vendor into libwui
+sed -i "s/libgui/libwui/g" "$LINEAGE_ROOT"/vendor/"$VENDOR"/"$DEVICE"/proprietary/vendor/lib/hw/camera.msm8937.so
